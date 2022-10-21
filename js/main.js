62f342657fde228148bb58bac9e27b34fb9eddc7
Muhammad Nabih <86538085+GOKERIAN@users.noreply.github.com>
2022-10-21 15:33:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 let mainImage = document.getElementById("master");
-let container = document.querySelector(".container")
+let container = document.querySelector(".container");
+let collspan = document.querySelector(".icon #collspan");
+
 function changePhone(phone) {
   mainImage.src = phone;
 }
-function changeColor(color){
-   container.style.backgroundColor=color
-
+function changeColor(color) {
+  container.style.backgroundColor = color;
 }
+
+function toggleLink() {
+  let theList = document.querySelector(" header ul");
+  
+  if (theList.style.display === "none") {
+    theList.style.display = "block";
+  } else {
+    theList.style.display = "none";
+  }
+}
+
+
+
+collspan.onclick = toggleLink;
